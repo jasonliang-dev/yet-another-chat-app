a651683f8df83d0d95c7950fcc3557defad3af20
jasonliang512 <jasonliang512@gmail.com>
2020-03-22 00:54:09 -0700

    remove mongodb-memory-server due to security vulnerability

--- server/config/mongoDB.js
+++ server/config/mongoDB.js
@@ -2,15 +2,16 @@
 /* eslint-disable no-console */
 
 import mongoose from 'mongoose';
-import { MongoMemoryServer } from 'mongodb-memory-server';
+// import { MongoMemoryServer } from 'mongodb-memory-server';
 
 // async because getConnectionString returns a promise, but
 // process.env.MONGODB_URI is just a string
 async function getURI() {
   if (process.env.NODE_ENV === 'test') {
     // reference: https://github.com/nodkz/mongodb-memory-server#usage
-    const mongod = new MongoMemoryServer();
-    return mongod.getConnectionString();
+    // const mongod = new MongoMemoryServer();
+    // return mongod.getConnectionString();
+    throw new Error('mongodb-memory-server has a security vulnerability');
   }
 
   return process.env.MONGODB_URI;
